8644f661a30af5760bda494fb99ea519c8a71ada
Wronqa <kuba.wrona@onet.pl>
2023-11-04 13:59:20 +0100

    Created functionality for like,comment and get specified user posta

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const uniqid = require('uniqid');
 
 const asyncErrorMiddleware = require('../middleware/asyncErrorMiddleware');
 const ErrorHandler = require('../tools/errorHandler');
@@ -20,4 +21,92 @@ exports.addPost = asyncErrorMiddleware(async (req, res) => {
 	const { updatedAt, __v, ...others } = savedPost.toJSON();
 
 	res.status(200).json({ status: true, message: others });
+});
+
+exports.timeline = asyncErrorMiddleware(async (req, res) => {
+	const username = req.username;
+
+	const userPosts = await Post.find({ username });
+
+	const currentUser = await User.findOne({ username });
+
+	const friendsPosts = await Promise.all(
+		currentUser.followings.map((username) => {
+			return Post.find({ username });
+		})
+	);
+
+	const posts = userPosts.concat(...friendsPosts);
+
+	const filteredPosts = posts.map((post) => {
+		const { __v, updatedAt, ...others } = post.toJSON();
+
+		return others;
+	});
+
+	res.status(200).json(filteredPosts);
+});
+
+exports.like = asyncErrorMiddleware(async (req, res) => {
+	const username = req.username;
+
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (post.likes.includes(username)) {
+			await post.updateOne({ $pull: { likes: username } });
+		} else {
+			await post.updateOne({ $push: { likes: username } });
+		}
+
+		res.status(200).json({ status: true, message: 'Post has been liked' });
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+exports.comment = asyncErrorMiddleware(async (req, res) => {
+	const username = req.username;
+
+	const comment = {
+		id: uniqid(),
+		user: username,
+		desc: req.body.desc,
+		date: new Date(),
+	};
+
+	const post = await Post.findByIdAndUpdate(
+		req.params.id,
+		{
+			$push: { comments: comment },
+		},
+		{ new: true }
+	);
+
+	res.status(200).json({ status: true, message: post });
+});
+
+exports.remove = asyncErrorMiddleware(async (req, res) => {
+	const username = req.username;
+
+	const post = await Post.findById(req.params.id);
+
+	if (post.username === username) {
+		post.deleteOne();
+		res.status(200).json({ status: true, message: 'Post has been deleted' });
+	} else {
+		req.status(403).json({ status: false, message: 'Unauthorized action' });
+	}
+});
+
+exports.userPosts = asyncErrorMiddleware(async (req, res) => {
+	const userPosts = await Post.find({ username: req.params.username });
+
+	const posts = userPosts.map((post) => {
+		const { __v, updatedAt, ...others } = post.toJSON();
+
+		return others;
+	});
+
+	res.status(200).json({ status: true, message: posts });
 });
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
-const { addPost } = require('../controllers/postController');
+const {
+	addPost,
+	timeline,
+	like,
+	comment,
+	remove,
+	userPosts,
+} = require('../controllers/postController');
 const verify = require('../middleware/authMiddleware');
 
 router.route('/post').post(verify, addPost);
+router.route('/post').get(verify, timeline);
+router.route('/post/like/:id').put(verify, like);
+router.route('/post/comment/:id').put(verify, comment);
+router.route('/post/:id').delete(verify, remove);
+router.route('/post/:username').get(verify, userPosts);
 
 module.exports = router;
